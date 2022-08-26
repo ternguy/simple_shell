d39d8cb9840878a8094c4eed2922f5aacf96c134
MG-Musty <mustaphaaliyugaladima@gmail.com>
2022-08-26 08:03:55 -0700

    my first shell readme

--- shell.h
+++ shell.h
@@ -172,6 +172,7 @@ void print_error(info_t *, char *);
 int print_d(int, int);
 char *convert_number(long int, int, int);
 void remove_comments(char *);
+void print_arg(char *arg);
 
 /* toem_builtin.c */
 int _myexit(info_t *);
