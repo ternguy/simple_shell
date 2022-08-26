20bc0cb55de6607d4e3dfd0879e9438d87fe3c06
MG-Musty <mustaphaaliyugaladima@gmail.com>
2022-08-26 08:02:22 -0700

    siimple code that passes betty

---
+++ betty.c
@@ -0,0 +1,11 @@
+#include "shell.h"
+#include <stdio.h>
+
+/**
+ * print_arg - Prints a string using printf
+ * @arg: The string to be printed
+ */
+void print_arg(char *arg)
+{
+	print_string(arg);
+}
